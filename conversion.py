43a41082ca3ebe5d7caee6e6d97658529dd8f8c3
nbcmguarin0 <michael.guarino@nbcuni.com>
2019-07-15 11:32:33 -0400

    scc to txt via PBS pycaption and pdf/doc/docx to txt/pkl functionality working

--- conversion.py
+++ conversion.py
@@ -4,15 +4,17 @@ import traceback
 from pycaption import SCCReader
 from tika import parser as tika_parser
 
-def get_output_file_path(src_file_path, desired_ext):
+def get_output_file_path(src_file_path, desired_ext, type_):
     file_name, file_ext = src_file_path.split("/")[-1].split(".")
-    return os.path.join("/".join(src_file_path.split("/")[:-1]), file_name + desired_ext)
+    if not os.path.exists(os.path.join("/".join(src_file_path.split("/")[:-1]), type_)):
+        os.mkdir(os.path.join("/".join(src_file_path.split("/")[:-1]), type_))
+    return os.path.join("/".join(src_file_path.split("/")[:-1]), type_, file_name + desired_ext)
 
 def parse_captions(scc_file_path, desired_ext):
     assert(desired_ext == ".txt"), "caption file must output to txt file"
     src_file = open(scc_file_path, "r")
     pycaps = SCCReader().read(src_file.read())
-    with open(get_output_file_path(scc_file_path, desired_ext), "w+") as output_file:
+    with open(get_output_file_path(scc_file_path, desired_ext, "cc_output"), "w+") as output_file:
         for lang, caption in pycaps._captions.items():
             if lang == "en-US":
                 for caption_at_timestep in caption:
@@ -25,9 +27,9 @@ def parse_doc_images(doc_img_file_path, desired_ext):
     assert(desired_ext[0] == ".txt" and desired_ext[1] == ".pkl"), "document image file must output " \
                                                                 "content to txt file and metadata info to pkl file"
     doc_img_parsed = tika_parser.from_file(doc_img_file_path)
-    with open(get_output_file_path(doc_img_file_path, desired_ext[0]), "w") as content_output_file:
+    with open(get_output_file_path(doc_img_file_path, desired_ext[0], "ocr_output_txt"), "w") as content_output_file:
         content_output_file.writelines(doc_img_parsed["content"])
-    with open(get_output_file_path(doc_img_file_path, desired_ext[1]), "w") as metadata_output_file:
+    with open(get_output_file_path(doc_img_file_path, desired_ext[1], "ocr_output_pkl"), "w") as metadata_output_file:
         metadata_output_file.writelines(doc_img_parsed["metadata"])
 
 if __name__ == "__main__":
@@ -36,6 +38,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for src_file_path in os.listdir(args.dir):
+        src_file_path = src_file_path.lower()
         try:
             full_src_file_path = os.path.join(args.dir, src_file_path)
             if src_file_path.endswith(".scc"):
